da8b028f87b526fe6a9dcdecc99d39c159fcf006
laurent.debreu1@gmail.com <laurent.debreu1@gmail.com>
2020-04-20 11:03:39 +0200

    cv

--- tools/WEIGHTS/src/scripgrid.F90
+++ tools/WEIGHTS/src/scripgrid.F90
@@ -6,21 +6,13 @@ PROGRAM scripgrid
 
   CHARACTER(char_len) :: infile
 
-#if defined ARGC
-  INTEGER :: IARGC
-  EXTERNAL IARGC
-
-  if (IARGC() == 1) then
-    CALL GETARG(1, infile)
-    CALL convert( infile )
-  ELSE
-    write(6,*) 'need to supply a namelist file'
-  ENDIF
-#else
+  if (COMMAND_ARGUMENT_COUNT() == 1) then
+    CALL GET_COMMAND_ARGUMENT(1, infile)
+  else
   write(6,*) 'enter name of namelist file'
   read(5,*) infile
+  endif
 
   CALL convert( infile )
-#endif
 
 END PROGRAM scripgrid
