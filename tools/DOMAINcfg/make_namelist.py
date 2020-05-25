8dbfebb0de91b519a5c688a870e7947195b368ba
rblod <rachid.benshila@legos.obs-mip.fr>
2020-05-25 11:39:03 +0200

    make_namelist with iperio and south closed

--- tools/DOMAINcfg/make_namelist.py
+++ tools/DOMAINcfg/make_namelist.py
@@ -5,10 +5,10 @@ import sys
 
 if len(sys.argv) == 1 :
    namfile='namelist_cfg'
-   topofile='GEBCO_2014_2D.nc' 
+   topofile='GEBCO_2020.nc' 
 elif len(sys.argv) == 2 :
    namfile=sys.argv[1]
-   topofile='GEBCO_2014_2D.nc' 
+   topofile='GEBCO_2020.nc' 
 elif len(sys.argv) == 3 :
    namfile=sys.argv[1]
    topofile=sys.argv[2] 
@@ -20,6 +20,11 @@ else:
 Agrilefile="AGRIF_FixedGrids.in"
 grid=[]
 nbghostcells  = 3
+nbghostcells_x = nbghostcells
+nbghostcells_y = nbghostcells
+nbghostcells_y_n = nbghostcells_y
+nbghostcells_y_s = nbghostcells_y
+
 
 with open(Agrilefile) as fp:
    line = fp.readline()
@@ -39,22 +44,33 @@ print("Found", len(grid), "grids", ":")
 
 f1="namelist_ref"
 
+
+
 cnt = 1
 for g in range(len(grid)) :
-    jpiglo = (int(grid[cnt-1][1])-int(grid[cnt-1][0]))*int(grid[cnt-1][4]) +2 +2*nbghostcells
-    jpjglo = (int(grid[cnt-1][3])-int(grid[cnt-1][2]))*int(grid[cnt-1][5]) +2 +2*nbghostcells
-    #print( "Grid "+str(cnt)+" : jpiglo = "+cnt(jpiglo)+ "  jpjglo = "+str(jpjglo) ) 
-    print('Grid {:1d} : jpiglo = {:3d} , jpjglo = {:3d}'.format(cnt, jpiglo, jpjglo))
-
+    
     f2 = open(str(cnt)+'_'+f1,'w')
     with open(f1) as fp:
         line = fp.readline()
         cnt1 = 1
         while line :
            line = fp.readline()
+           if line.strip().startswith('jpiglo'):
+               jpi_parent=line.strip().split()[2]               
            f2.write(line)
            cnt1 += 1
     f2.close()
+
+    if int(grid[cnt-1][2]) == 1:
+        nbghostcells_y_s = 0
+    if int(grid[cnt-1][1]) + int(grid[cnt-1][0]) == int(jpi_parent) :
+        nbghostcells_x = 0
+
+    jpiglo = (int(grid[cnt-1][1])-int(grid[cnt-1][0]))*int(grid[cnt-1][4]) +2 +2*nbghostcells_x
+    jpjglo = (int(grid[cnt-1][3])-int(grid[cnt-1][2]))*int(grid[cnt-1][5]) +2 + nbghostcells_y_n  + nbghostcells_y_s
+    #print( "Grid "+str(cnt)+" : jpiglo = "+cnt(jpiglo)+ "  jpjglo = "+str(jpjglo) ) 
+    print('Grid {:1d} : jpiglo = {:3d} , jpjglo = {:3d}'.format(cnt, jpiglo, jpjglo))
+
     f2 = open(str(cnt)+'_'+namfile,'w')
     with open(namfile) as fp:
         line = fp.readline()
@@ -62,7 +78,10 @@ for g in range(len(grid)) :
         while line :
            line = fp.readline()
            if line.strip().startswith('jperio'):
-               line = "   jperio = 0\n"
+               if int(grid[cnt-1][1]) + int(grid[cnt-1][0]) == int(jpi_parent) :
+                   line = "   jperio = 1\n"
+               else:
+                   line = "   jperio = 0\n"
            if line.strip().startswith('nn_bathy'):
            	   line = "   nn_bathy = 2\n"
            if line.strip().startswith('nn_interp'):
@@ -86,8 +105,10 @@ for g in range(len(grid)) :
            if line.strip().startswith('jpjdta'):
            	   line = "   jpjdta = "+str(jpjglo)+"\n"
            if line.strip().startswith('cp_cfg'):
-           	   line = "   cp_cfg = 'dumb'\n"      
-
+           	   line = "   cp_cfg = 'dumb'\n"                 
+           if nbghostcells_y_s == 0 :
+               if line.strip().startswith('south_boundary_open'):
+                   line = "   south_boundary_open = .FALSE.\n"                              
            f2.write(line)
            cnt1 += 1
     f2.close()     
